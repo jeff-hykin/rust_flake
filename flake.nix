1ddc996a3afcdfae154a835b87b71930932455df
Jeff Hykin <jeff.hykin@gmail.com>
2025-08-25 17:13:53 -0500

    v1.85.0

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
         fenix.url = "github:nix-community/fenix";
         fenix.inputs.nixpkgs.follows = "nixpkgs";
         rust-manifest = {
-            url = "https://static.rust-lang.org/dist/2025-01-30/channel-rust-1.84.1.toml";
+            url = "https://static.rust-lang.org/dist/2025-02-20/channel-rust-1.85.0.toml";
             flake = false;
         };
     };
@@ -28,10 +28,10 @@
                     lib = {
                         rustPlatform = rustPlatform // {
                             info = {
-                                version = "1.84.1";
+                                version = "1.85.0";
                                 channel = "stable";
-                                manifestUrl = "https://static.rust-lang.org/dist/2025-01-30/channel-rust-1.84.1.toml";
-                                date = "2025-01-30"; 
+                                manifestUrl = "https://static.rust-lang.org/dist/2025-02-20/channel-rust-1.85.0.toml";
+                                date = "2025-02-20"; 
                             };
                         };
                     };
